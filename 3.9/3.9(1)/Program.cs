eb66ae19327038bf7f00d2aa5b456d9bea45288b
TreepoBear <956374856@qq.com>
2020-03-10 21:38:45 +0800

    the forth week

---
+++ 3.9/3.9(1)/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._9_1_
+{
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> temp = head;
+            while (temp.Next != null)
+            {
+                action(temp.Data);
+                temp = temp.Next;
+            }
+            action(temp.Data);
+        }
+    }
+
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            //赋值
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            //打印元素
+            Console.WriteLine("打印元素：");
+            intlist.ForEach(i => Console.WriteLine(i));
+            //求最大值
+            Console.WriteLine("求最大值:");
+            int max = 0;
+            intlist.ForEach(delegate (int i) { if (max < i) max = i; });
+            Console.WriteLine(max);
+            //求和
+            Console.WriteLine("求和:");
+            int sum = 0;
+            intlist.ForEach(i => sum += i);
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3.9/Clock/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    //声明委托格式
+    public delegate void ClockSoundHandler();
+    public delegate void AlarmHandler();
+
+    class Clock
+    {
+        //定义事件
+        public event ClockSoundHandler TickEvent;
+        public event ClockSoundHandler TockEvent;
+        public event AlarmHandler TimeUpEvent;
+
+        //时钟时间
+        private static int Sec { get; set; }
+        private static int Min { get; set; }
+        private static int Hour { get; set; }
+        //响铃时间
+        public int s;
+        public int m;
+        public int h;
+        //构造函数
+        public Clock(int h,int m,int s)
+        {
+            Sec = 0;
+            Min = 0;
+            Hour = 0;
+            this.s = s;
+            this.m = m;
+            this.h = h;
+            TickEvent += onTick;
+            TockEvent += onTock;
+            TimeUpEvent += alarm;
+        }
+
+        void onTick() {Console.WriteLine("~~~~~~~Tick~~~~~~~");}
+        void onTock() { Console.WriteLine("~~~~~~~Tock~~~~~~~"); }
+        void alarm() { Console.WriteLine("-----Time's Up-----"); }
+
+        public void StartWork()
+        {
+            while (Sec+1 != s || Min != m || Hour != h)
+            {
+                Sec += 1;
+                if (Sec == 60 && Min != 60)
+                {
+                    Min += 1;
+                    Sec = 0;
+                    TockEvent();
+                    Thread.Sleep(1000);
+                    continue;
+                }
+                else if (Sec == 60 && Min == 60)
+                {
+                    Hour += 1;
+                    Sec = Min = 0;
+                    TockEvent();
+                    Thread.Sleep(1000);
+                    continue;
+                }
+                TickEvent();
+                Thread.Sleep(1000);
+            }
+            TimeUpEvent();
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock1 = new Clock(0,0,5);
+            clock1.StartWork();
+            Console.ReadKey();
+        }
+    }
+}
