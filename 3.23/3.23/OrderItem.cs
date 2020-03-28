83866166ab667f241147e971bbc082953d83209f
TreepoBear <956374856@qq.com>
2020-03-28 19:36:20 +0800

    sixth week

---
+++ 3.23/3.23/OrderItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._23
+{
+    public class OrderItem
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int ProductNum { get; set; }
+        public int Price { get; set; }
+        public static ProductDB db;
+
+        public OrderItem() { }
+
+        public OrderItem(string name, int num)
+        {
+            db = new ProductDB();
+            this.ProductName = name;
+            this.ProductNum = num;
+            this.ProductID = db.idlist[name];
+            this.Price = db.pricelist[name];
+        }
+
+        public override string ToString()
+        {
+            return ProductID + " " + ProductName + "  $" + Price + " *" + ProductNum;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as OrderItem;
+            return item != null &&
+                   ProductID == item.ProductID &&
+                   ProductName == item.ProductName &&
+                   ProductNum == item.ProductNum &&
+                   Price == item.Price;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 746136198;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(ProductID);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(ProductName);
+            hashCode = hashCode * -1521134295 + ProductNum.GetHashCode();
+            hashCode = hashCode * -1521134295 + Price.GetHashCode();
+            return hashCode;
+        }
+    }
+}
---
+++ 3.23/3.23/ProductDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._23
+{
+    public class ProductDB
+    {
+        public Dictionary<string, int> pricelist = new Dictionary<string, int>();
+        public Dictionary<string, string> idlist = new Dictionary<string, string>();
+        public ProductDB()
+        {
+            pricelist.Add("orange", 4);
+            pricelist.Add("apple", 6);
+            pricelist.Add("bicycle", 250);
+            pricelist.Add("book", 30);
+            pricelist.Add("tea", 15);
+            pricelist.Add("pencil", 3);
+            pricelist.Add("notebook", 8);
+            pricelist.Add("sausage", 5);
+            pricelist.Add("air-conditioner", 580);
+
+            idlist.Add("orange", "P00001");
+            idlist.Add("apple", "P00002");
+            idlist.Add("bicycle", "P00003");
+            idlist.Add("book", "P00004");
+            idlist.Add("tea", "P00005");
+            idlist.Add("pencil", "P00006");
+            idlist.Add("notebook", "P00007");
+            idlist.Add("sausage", "P00008");
+            idlist.Add("air-conditioner", "P00009");
+        }
+    }
+}
